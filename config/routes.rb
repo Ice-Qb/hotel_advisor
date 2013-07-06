ab14e94605d63512fa5f1e2e0ed973cc1fbbdc4a
Ihor Strumetsky <llighterr@gmail.com>
2013-07-06 09:50:46 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 HotelAdvisor::Application.routes.draw do
 
-
   root :to => 'hotels#top'
+  
   resources :users
   resources :hotels
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
